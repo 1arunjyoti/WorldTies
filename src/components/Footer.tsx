7123430bfff4ba3c374912cafc35d5e4c45fc307
ARUNJYOTI DEBNATH <155171660+1arunjyoti@users.noreply.github.com>
2025-06-25 12:17:08 +0530

    more relation data added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="text-center p-2 text-xs text-gray-500 border-t border-gray-700">
-      <p>Data is for demonstrative purposes only. WorldTies is not responsible for any misuse of the data. Updated on: 23rd June 2025</p>
+    <footer className="text-center p-2 sm:p-3 text-[10px] sm:text-xs text-gray-500 border-t border-gray-700 dark:border-gray-600">
+      <p className="px-2">Data is for demonstrative purposes only. WorldTies is not responsible for any misuse of the data. Last updated: 23rd June 2025</p>
     </footer>
   );
 };
